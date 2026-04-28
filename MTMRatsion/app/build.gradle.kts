bc0d63ecc1d54c34ea2a6a74934ad279cb5b3ceb
DeWeWO <dewel000per@gmail.com>
2026-04-29 00:42:10 +0500

    Fix JVM 17 target compatibility

--- MTMRatsion/app/build.gradle.kts
+++ MTMRatsion/app/build.gradle.kts
@@ -96,6 +96,11 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
